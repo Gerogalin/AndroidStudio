76602daa3ecc86f2ac535c5e202aba7b2898a0d3
Jeven Gerogalin <100065163+Gerogalin@users.noreply.github.com>
2023-03-08 20:49:10 +0800

    Create MainActivity.java

---
+++ InsertRecord/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.insertrecord;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button CreateNewEmp, ManageRecords;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CreateNewEmp = (Button) findViewById(R.id.CreateNewEmp);
+        ManageRecords = (Button) findViewById(R.id.ManageRecords);
+
+        CreateNewEmp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent in = new Intent(MainActivity.this,CreateNewCostumer.class);
+                startActivity( in);
+            }
+        });
+        ManageRecords.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent in = new Intent(MainActivity.this,ManageRecords.class);
+                startActivity( in);
+            }
+        });
+    }
+}
